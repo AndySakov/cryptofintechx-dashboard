415edaeaca7eeeab1f5783173467516cce359eb0
AndySakov <andysakov1958@gmail.com>
2022-01-18 20:36:05 +0100

    Updated axios instance headers

--- src/app/axiosInstance.js
+++ src/app/axiosInstance.js
@@ -1,9 +1,11 @@
-const axios = require('axios').default;
+import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://5u35m7cyajmu4s57.cryptofintechx.com/api',
     timeout: 5000,
     headers: {
-        'X-API-Key': process.env.API_KEY
+        'X-API-Key': process.env.API_KEY,
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
     }
 });
